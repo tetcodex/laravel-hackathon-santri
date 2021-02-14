16f15e38f3ab2d0172b88858d3bab078ea81d573
tetcodex <ulinnuha.deni@gmail.com>
2021-02-14 08:09:37 +0700

    edit sidebar and tab title

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,7 +3,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/dashboard">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/company">
                 <div class="sidebar-brand-text mx-3">Karsa App <sup>Company</sup></div>
             </a>
 
